3d26a4846c3150ec953c72fbefb46b5a46b45666
Mateusz <mateusz.macieja8@gmail.com>
2016-11-18 09:35:01 +0100

    Playing with templates. Now diod class needs only two arguments to be initialized.

---
+++ diod.h
@@ -0,0 +1,81 @@
+/*
+ * diod.h
+ *
+ *  Created on: Nov 18, 2016
+ *      Author: matitechno
+ */
+
+#ifndef DIOD_H_
+#define DIOD_H_
+
+#include <avr/io.h>
+
+class DiodBase
+{
+public:
+	DiodBase(const DiodBase&) = delete;
+	DiodBase& operator=(const DiodBase&) = delete;
+
+	void on() const
+	{
+		*dataReg |= (1<<pin);
+	}
+	void off() const
+	{
+		*dataReg &= ~(1<<pin);
+	}
+	void toggle() const
+	{
+		*dataReg ^= (1<<pin);
+	}
+
+protected:
+	DiodBase(const uint8_t pin, volatile uint8_t* const dataReg,
+			volatile uint8_t* const dataDirReg):
+				pin(pin),
+				dataReg(dataReg),
+				dataDirReg(dataDirReg)
+{
+		*dataDirReg |= (1<<pin);
+}
+
+	const uint8_t pin;
+	volatile uint8_t* const dataReg;
+	volatile uint8_t* const dataDirReg;
+};
+
+template<typename T>
+class Diod;
+
+class PortB{};
+class PortC{};
+class PortD{};
+
+template<>
+class Diod<PortB>: public DiodBase
+{
+public:
+	Diod(const uint8_t pin):
+		DiodBase(pin, &PORTB, &DDRB)
+{}
+};
+
+template<>
+class Diod<PortC>: public DiodBase
+{
+public:
+	Diod(const uint8_t pin):
+		DiodBase(pin, &PORTC, &DDRC)
+{}
+};
+
+template<>
+class Diod<PortD>: public DiodBase
+{
+public:
+	Diod(const uint8_t pin):
+		DiodBase(pin, &PORTD, &DDRD)
+{}
+};
+
+#endif /* DIOD_H_ */
--- main.cpp
+++ main.cpp
@@ -1,50 +1,14 @@
-extern "C"
-{
 #include <avr/io.h>
 #include <util/delay.h>
-}
-
-// using pointers instead of references becuase of warning
-class Diod
-{
-public:
-	Diod(volatile uint8_t* dataDirReg, volatile uint8_t* dataReg, uint8_t pin):
-		dataDirReg(dataDirReg),
-		dataReg(dataReg),
-		pin(pin)
-{
-		*dataDirReg |= (1<<pin);
-}
-	Diod(const Diod&) = delete;
-	Diod& operator=(const Diod&) = delete;
-
-	void toggle()
-	{
-		*dataReg ^= (1<<pin);
-	}
-	void on()
-	{
-		*dataReg |= (1<<pin);
-	}
-	void off()
-	{
-		*dataReg &= ~(1<<pin);
-	}
-
-private:
-	volatile uint8_t* dataDirReg;
-	volatile uint8_t* dataReg;
-	const uint8_t pin;
-};
-
+#include "diod.h"
 
 int main()
 {
-	Diod diod1(&DDRB, &PORTB, PB1);
+	Diod<PortB> diod(PB1);
 
 	for(;;)
 	{
-		diod1.toggle();
+		diod.toggle();
 		_delay_ms(1000);
 	}
 }
